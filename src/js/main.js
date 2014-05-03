61ba4fdfebb26af2a4db908647e92d6fda1184a6
luis <lrosario101@gmail.com>
2014-05-03 02:19:35 -0400

    new technique. insta-rollover

--- src/js/main.js
+++ src/js/main.js
@@ -1,16 +1,16 @@
 //wait until main document is loaded
 window.addEventListener('load',function(){
   // add hover over effect
-  var imgs = document.querySelectorAll('#team-list img');
-  var dir = 'img/galacticod/';
+  // var imgs = document.querySelectorAll('#team-list img');
+  // var dir = 'img/galacticod/';
 
-  for (var i = 0; i < imgs.length; i++) {
-  	imgs[i].onmouseover = function() {
-  		this.src = dir + this.getAttribute('data-fish-face');
-  	}
-  	imgs[i].onmouseout = function() {
-  		this.src = dir + this.getAttribute('data-normal-face');
-  	}
+  // for (var i = 0; i < imgs.length; i++) {
+  // 	imgs[i].onmouseover = function() {
+  // 		this.src = dir + this.getAttribute('data-fish-face');
+  // 	}
+  // 	imgs[i].onmouseout = function() {
+  // 		this.src = dir + this.getAttribute('data-normal-face');
+  // 	}
 
-  };
+  // };
 }); //end addEventListener
