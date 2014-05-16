176c9c2722051f8864bbbc041037be9b432ef644
luis <lrosario101@gmail.com>
2014-05-15 23:46:38 -0400

    different method for rollovers

--- src/js/main.js
+++ src/js/main.js
@@ -1,16 +1,22 @@
-//wait until main document is loaded
 window.addEventListener('load',function(){
   // add hover over effect
-  // var imgs = document.querySelectorAll('#team-list img');
-  // var dir = 'img/galacticod/';
+  var fishFaces = document.querySelectorAll('.profile-pic div');
+  for (var i = 0; i < fishFaces.length; i++) {
+    
+    var url = fishFaces[i].getAttribute('data-fish')
+    fishFaces[i].style.backgroundImage = "url('" + url + "')";
+  };
 
-  // for (var i = 0; i < imgs.length; i++) {
-  // 	imgs[i].onmouseover = function() {
-  // 		this.src = dir + this.getAttribute('data-fish-face');
-  // 	}
-  // 	imgs[i].onmouseout = function() {
-  // 		this.src = dir + this.getAttribute('data-normal-face');
-  // 	}
-
-  // };
-}); //end addEventListener
+  $('a[href*=#]:not([href=#])').click(function() {
+    if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+      var target = $(this.hash);
+      target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+      if (target.length) {
+        $('html,body').animate({
+          scrollTop: target.offset().top
+        }, 3000);
+        return false;
+      }
+    }
+  });
+});
